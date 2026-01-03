ce2cb5ecbf80d4d1aaab37e42ee3ccd47b61e867
avengsum <sy710481@gmail.com>
2026-01-03 15:33:18 +0530

    learn method

--- class.py
+++ class.py
@@ -5,11 +5,38 @@ class car:
     self.model = model
     self.speed = speed
   
-  def show_car(self):
+  def show_info(self):
     print(f"speed: {self.speed} model:{self.model}")
 
 car1 = car("tesla","80km")
-car1.show_car()
 car2 = car("bmw","90km")
-car2.show_car()
+
+class Book:
+  def __init__(self,title,page):
+    self.title = title
+    self.page = page
+    print("Book created")
+
+
+class Account:
+  def __init__(self,name,balance):
+    self.name= name
+    self.balance = balance
+
+  def deposit(self,amount):
+    self.balance= amount 
+    print(f"remaing balance after deposit : {amount}")
+  
+  def withdraw(self,amount):
+    if amount > self.balance:
+      print("Insufficient balance") 
+    else:
+      self.balance = amount
+      print(f"remaing balance after deposit : {amount}")
+
+a1 = Account("suraj","50000")
+
+a1.withdraw("60000")
+
+
 
